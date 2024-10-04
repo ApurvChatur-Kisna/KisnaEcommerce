d2b01530bd51f134299a2301dc647f83a6ce1b7b
ApurvChatur-Kisna <apurv.chatur.17kisna@gmail.com>
2024-10-04 12:30:45 +0530

    (fix): Again PWA fixed with proper manifest

--- site/frontend/vite.config.ts
+++ site/frontend/vite.config.ts
@@ -4,36 +4,49 @@ import { defineConfig } from "vite"
 import { VitePWA } from 'vite-plugin-pwa'
 
 
-export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({ 
-      manifest: {
-        // "registerType": 'autoUpdate',
-        "name":"Abol",
-        "short_name":"Abol",
-        "display": "standalone",
-        "background_color": "#4A274F",
-        "theme_color": "#4A274F",
-        "icons":[
-          {
-            src: "logo.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-          // {
-          //   src: "Business/Abol/favicon_io/android-chrome-192x192.png",
-          //   sizes: "192x192",
-          //   type: "image/png",
-          // },
-        ],
-      } ,
-      registerType: 'autoUpdate',
-      // devOptions: {
-      //   enabled: true
-      // } 
-    })
+const manifestForPlugIn = {
+  registerType:'prompt',
+  includeAssests:['favicon.ico', "apple-touc-icon.png", "masked-icon.svg"],
+  manifest:{
+    name:"KISNA E-commerce",
+    short_name:"KISNA E-com",
+    description:"Diamond Jewellery",
+    icons:[{
+      src: 'logo.png',
+      sizes:'192x192',
+      type:'image/png',
+      purpose:'favicon'
+    },
+    {
+      src:'logo.png',
+      sizes:'512x512',
+      type:'image/png',
+      purpose:'favicon'
+    },
+    {
+      src: 'logo.png',
+      sizes:'180x180',
+      type:'image/png',
+      purpose:'apple touch icon',
+    },
+    {
+      src: 'logo.png',
+      sizes:'512x512',
+      type:'image/png',
+      purpose:'any maskable',
+    }
   ],
+  theme_color:'#171717',
+  background_color:'#f0e7db',
+  display:"standalone",
+  scope:'/',
+  start_url:"/",
+  orientation:'portrait'
+  }
+}
+
+export default defineConfig({
+  plugins: [ react(), VitePWA(manifestForPlugIn as Object) ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
