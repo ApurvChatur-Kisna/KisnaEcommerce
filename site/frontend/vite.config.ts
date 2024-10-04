3ab96e43b962a60eda7fa4d59eb078cc1cb326b1
ApurvChatur-Kisna <apurv.chatur.17kisna@gmail.com>
2024-10-04 12:33:41 +0530

    (assets): Public folder resources added

--- site/frontend/vite.config.ts
+++ site/frontend/vite.config.ts
@@ -12,25 +12,25 @@ const manifestForPlugIn = {
     short_name:"KISNA E-com",
     description:"Diamond Jewellery",
     icons:[{
-      src: 'logo.png',
+      src: '/android-chrome-192x192.png',
       sizes:'192x192',
       type:'image/png',
       purpose:'favicon'
     },
     {
-      src:'logo.png',
+      src:'/android-chrome-512x512.png',
       sizes:'512x512',
       type:'image/png',
       purpose:'favicon'
     },
     {
-      src: 'logo.png',
+      src: '/apple-touch-icon.png',
       sizes:'180x180',
       type:'image/png',
       purpose:'apple touch icon',
     },
     {
-      src: 'logo.png',
+      src: '/maskable_icon.png',
       sizes:'512x512',
       type:'image/png',
       purpose:'any maskable',
